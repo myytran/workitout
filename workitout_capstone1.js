c0f4fe9df9c06d49bf2c54bc6096d8bce434eab2
My Tran <myytran@thinkful.com>
2018-01-31 14:34:54 -0500

    Updates to embed and index.html

--- workitout_capstone1.js
+++ workitout_capstone1.js
@@ -3,16 +3,17 @@ const ytURL= 'https://www.googleapis.com/youtube/v3/search'; // API key
 const params = { //parameters in object
   'part': 'snippet',
   'key':'AIzaSyB2GqU6ctQPiYz9131aSb6w1_AZCXmZV_A',
+  'type': 'video',
   'q': 'workouts',
   'maxResults': 50,
   'ID':0 //nextPageToken, need to call for next page so 50 results don't repeat
 }
 
 $(document).ready(function() {
-  $('.search').click(function(e) {
-
+  $('.search-form').submit(function(e) {
+    e.preventDefault();
     var query = $('.query').val();
-    params.q= query;
+    params.q= query + " workout";
     searchYouTube();
     $('.query').val(''); //clears out query after searchYouTube runs
   });
@@ -22,6 +23,13 @@ $(document).ready(function() {
   });
 });
 
+/*$('#play-video').on('click', function(ev) {
+
+   $("#video")[0].src += "&autoplay=1";
+   ev.preventDefault();
+
+ });
+*/
 function searchYouTube() {
   $.getJSON(ytURL,params,function(data){
 
@@ -31,11 +39,29 @@ function searchYouTube() {
   });
 };
 
+
+
+
 function pickRandomVideo(){
    $('.results').html('');
-  var ran = Math.floor(Math.random() * results.items.length) + 1;
+   if(results.items.length>0){
+      var ran = Math.floor(Math.random() * results.items.length) + 1;
 
 
-      $('.results').append('<p><h2>'+results.items[ran].snippet.title+'</h2>');
-      $('.results').append('<img src="'+results.items[ran].snippet.thumbnails.high.url+'"/></p>');
+        /*  $('.results').append('<p><h2>'+results.items[ran].snippet.title+'</h2>');
+          $('.results').append('<img src="'+results.items[ran].snippet.thumbnails.high.url+'"/></p>');*/
+        $('.results').append('<iframe width="560" height="315" src="https://www.youtube.com/embed/'+results.items[ran].id.videoId+'?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+
+        results.items.splice(ran,1);
+      } else {
+        $('.results').html('<h2>Sorry, you have run out of options, try a new search</h2>');
+      }
 }
+
+/*<div class="youtubeVid">' +
+  results.snippet.title +  '<br/>' +
+  '<a href="https://www.youtube.com/watch?v=' +
+  value.id.videoId +
+  '"><img class="youtubeVid" src="' +
+  results.items[ran].snippet.thumbnails.high.url+ '"></a>' + '<br/>' +
+  results.items[ran].snippet.description + '</li>'*/
